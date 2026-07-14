eca02364e290f99ac585d7b63a89c31034001ac7
Le_Roux-nard <65075968+Le-Roux-nard@users.noreply.github.com>
2026-07-14 11:54:47 +0200

    refactor

--- src/crousApi.ts
+++ src/crousApi.ts
@@ -16,9 +16,9 @@ import { RestaurantJson } from "./classes/RestaurantJSON.js";
 class CrousAPI {
 	static isLoaded: boolean = false;
 	private static liensDatasets: string[] = [
-		"https://www.data.gouv.fr/api/2/datasets/5548d35cc751df0767a7b26c/resources/?page=1&type=main&page_size=-1", //Actualites
-		"https://www.data.gouv.fr/api/2/datasets/5548d994c751df32e0a7b26c/resources/?page=1&type=main&page_size=-1", //Résidences
-	];
+		"5548d35cc751df0767a7b26c", // Actualités
+		"5548d994c751df32e0a7b26c"  // Résidences
+	].map(datasetId => `https://www.data.gouv.fr/api/2/datasets/${datasetId}/resources/?page=1&type=main&page_size=999999`)
 
 	private static cache?: CrousAPI;
 
@@ -58,10 +58,12 @@ class CrousAPI {
 		for (const url of CrousAPI.liensDatasets) {
 			let promise = new Promise<void>(async (resolve) => {
 				let data = await fetch(url).then((r) => r.json().then((jsonBody) => jsonBody.data));
-
 				//#region Récupération des données
 				for await (const dataset of data as Dataset[]) {
-					let result = /^(?<type>.+?)(?=(?: du)? CROUS)(?:.+)(?<=CROUS (?:de |du |d'| )?)(?<crous>.+)/gim.exec(dataset.title);
+					// let result = /^(?<type>.+?)(?=(?: du)? CROUS)(?:.+)(?<=CROUS (?:de |du |d'| )?)(?<crous>.+)/gim.exec(dataset.title);
+					let result = /^(?<type>.+?)(?=(?: du)? CROUS)(?:.+)(?<=CROUS (?:de (?:(?:les?|la) )?|du |d'| +)?)(?<crous>.+)/gim.exec(
+						dataset.title
+					);
 					if (result?.groups) {
 						const nomCrous = result.groups.crous;
 						const idCrous = transformCrousName(trimLowSnakeEscape(nomCrous));
